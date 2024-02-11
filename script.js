2c819abaca174a0d7a106f999504f7264bd6bcdc
sahinomeerr <os202422@gmail.com>
2024-02-12 02:54:30 +0300

    a weather app

---
+++ script.js
@@ -0,0 +1,75 @@
+const apiKey = "90985cd0635423f09f09812bfd58d073";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+
+const inputBox = document.querySelector(".input-box input");
+const searchButton = document.querySelector(".input-box button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function getWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}&units=metric`);
+  if (response.status === 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error-text p").style.display = "block";
+
+    inputBox.addEventListener("click", () => {
+      document.querySelector(".error-text p").style.display = "none";
+    });
+  } else {
+    document.querySelector(".weather").style.display = "block";
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerText = data.name;
+    document.querySelector(".wind").innerText = data.wind.speed + "km/h";
+    document.querySelector(".temp").innerText =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerText = data.main.humidity + "%";
+
+    switch (data.weather[0].main) {
+      case "Clear":
+        weatherIcon.src = "images/clear.png";
+        break;
+      case "Clouds":
+        weatherIcon.src = "images/clouds.png";
+        break;
+      case "Rain":
+        weatherIcon.src = "images/rain.png";
+        break;
+      case "Drizzle":
+        weatherIcon.src = "images/drizzle.png";
+        break;
+      case "Snow":
+        weatherIcon.src = "images/snow.png";
+        break;
+      case "Mist":
+        weatherIcon.src = "images/mist.png";
+        break;
+      default:
+        weatherIcon.src = "images/unknown_weather.png";
+    }
+  }
+}
+
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetch(
+        `http://api.openweathermap.org/geo/1.0/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&limit=1&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          getWeather(data[0].name);
+        });
+    });
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  if (inputBox.value !== "") {
+    getWeather(inputBox.value);
+  } else {
+    alert("Please enter a city name");
+  }
+  inputBox.value = "";
+});
